95d513a2dda6c1086ba416d2b640c62bd3b99ab2
Clark Pegg <cpegg@uoguelph.ca>
2023-03-09 21:32:52 -0500

    added home screen

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,99 @@
 import 'package:flutter/material.dart';
 
+const exampleFiles = [
+  "abc",
+  "def",
+  "a file name",
+  "two three four",
+  "!!!!!!!!"
+];
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Hello there"));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: FittedBox(
+                child: IconButton(
+                    icon: const Icon(Icons.menu, color: Colors.black),
+                    onPressed: () => Text("1")),
+              ),
+            ),
+            const Expanded(
+              flex: 14,
+              child: TextField(
+                style: TextStyle(color: Colors.white70),
+                decoration: InputDecoration(
+                  hintText: "SEARCH",
+                  hintStyle: TextStyle(color: Colors.white70),
+                  filled: true,
+                  fillColor: Colors.black87,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: FittedBox(
+                child: IconButton(
+                  icon: Icon(Icons.settings, color: Colors.black),
+                  onPressed: () => Text("1"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: GridView.count(
+          crossAxisCount: 3, children: getFilesFromNames(exampleFiles)),
+    );
+  }
+}
+
+List<File> getFilesFromNames(List<String> names) {
+  List<File> files = [];
+
+  for (var name in names) {
+    files.add(File(name));
+  }
+
+  return files;
+}
+
+class File extends StatelessWidget {
+  final String filename;
+
+  const File(this.filename);
+
+  @override
+  Widget build(BuildContext context) {
+    return MenuItemButton(
+      onPressed: () => {},
+      child: Column(
+        children: [
+          const Expanded(
+            flex: 8,
+            child: FittedBox(
+              child: Icon(
+                Icons.file_present,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Text(
+              filename,
+              style: const TextStyle(color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+
 import './signup_page.dart';
 
 class LoginPage extends StatefulWidget {
